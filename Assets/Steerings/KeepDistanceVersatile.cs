333e5d43ae84f0d25e6a6ee1b1ebe65610b99cdd
wextia <valcazar96@gmail.com>
2017-01-25 11:16:01 +0200

    :moyai:

--- Assets/Steerings/KeepDistanceVersatile.cs
+++ Assets/Steerings/KeepDistanceVersatile.cs
@@ -6,33 +6,37 @@ namespace Steerings
 
     public class KeepDistanceVersatile : SteeringBehaviour
     {
-        public GameObject target;
+        [SerializeField]
+        private GameObject target;
         public float requiredDistance;
-        public static float alpha = 125f;
-        public static GameObject surrogateTarget = null;
+        [SerializeField]
+        public float alpha = 45;
+        private static GameObject surrogateTarget = null;
 
         public override SteeringOutput GetSteering()
         {
 
-            // no KS? get it
-            if (this.ownKS == null) this.ownKS = GetComponent<KinematicState>();
-
-            return KeepDistance.GetSteering(this.ownKS, this.target, this.requiredDistance);
+            if (ownKS == null)
+                ownKS = GetComponent<KinematicState>();
+            return KeepDistanceVersatile.GetSteering(ownKS, target, requiredDistance, alpha);
         }
 
-        public static SteeringOutput GetSteering(KinematicState ownKS, GameObject target, float requieredDistance)
+        public static SteeringOutput GetSteering(KinematicState ownKS, GameObject target,
+            float requieredDistance, float alpha)
         {
+            // Y axis of the target
+            Vector3 targetUp = target.transform.up;
+            // We rotate the Y vector alpha angles on the Z axis
+            Vector3 rotatedDir = Quaternion.Euler(0, 0, alpha) * targetUp;
+            // We add the position of the target to the direction multiplied
+            // by the distance to keep from the target
+            Vector3 desiredPos = target.transform.position + (rotatedDir * requieredDistance);
 
-            Vector3 directionFromTarget = (ownKS.position - target.transform.position).normalized;
-            Vector3 desiredPosition = target.transform.position + directionFromTarget * requieredDistance;
-            desiredPosition = Quaternion.Euler(0, 0, alpha) * desiredPosition;
-       
-
-            if (KeepDistance.surrogateTarget == null)
+            if (KeepDistanceVersatile.surrogateTarget == null)
             {
-                surrogateTarget = new GameObject("surrogateee");
+                KeepDistanceVersatile.surrogateTarget = new GameObject("surrogateee");
             }
-            surrogateTarget.transform.position = desiredPosition;
+            KeepDistanceVersatile.surrogateTarget.transform.position = desiredPos;
 
             return Seek.GetSteering(ownKS, surrogateTarget);
         }
